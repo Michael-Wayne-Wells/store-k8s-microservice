ca2bd24f325c3754363708370a6de788438314cd
Michael Wells <mwells1286@gmail.com>
2020-05-11 13:46:52 -0700

    add mondodb for users.

--- auth/src/index.ts
+++ auth/src/index.ts
@@ -1,11 +1,14 @@
 import express from 'express';
+import 'express-async-errors';
 import { json } from 'body-parser';
+import mongoose from 'mongoose';
 import { currentUserRouter } from './routes/current-user';
 import { signinRouter } from './routes/signin';
 import { signupRouter } from './routes/signup';
 import { signoutRouter } from './routes/signout';
 import { errorHandler } from './middlewars/error-handler';
 import { NotFoundError } from './errors/not-found-errors';
+
 const app = express();
 app.use(json());
 
@@ -13,11 +16,24 @@ app.use(currentUserRouter);
 app.use(signinRouter);
 app.use(signoutRouter);
 app.use(signupRouter);
-app.all('*', () => {
+app.all('*', async (req, res) => {
   throw new NotFoundError();
 });
 app.use(errorHandler);
+const start = async () => {
+  try {
+    mongoose.connect('mongodb://auth-mongo-srv:27017/auth', {
+      useNewUrlParser: true,
+      useUnifiedTopology: true,
+      useCreateIndex: true,
+    });
+    console.log('connected to mongo');
+  } catch (err) {
+    console.error(err);
+  }
+  app.listen(3000, () => {
+    console.log('listening on 3000!!!!');
+  });
+};
 
-app.listen(3000, () => {
-  console.log('listening on 3000!!!!');
-});
+start();
