11a7fbf33cf868e0247a3ceb2cc4b23c17598650
Michael Wells <mwells1286@gmail.com>
2020-05-10 21:36:26 -0700

    Add custom error handleing

--- auth/src/middlewars/error-handler.ts
+++ auth/src/middlewars/error-handler.ts
@@ -1,13 +1,16 @@
 import { Request, Response, NextFunction } from 'express';
+import { CustomError } from '../errors/CustomError';
 export const errorHandler = (
   err: Error,
   req: Request,
   res: Response,
   next: NextFunction
 ) => {
-  console.log('something went wrong', err);
+  if (err instanceof CustomError) {
+    return res.status(err.statusCode).send({ errors: err.serializeErrors() });
+  }
 
   res.status(400).send({
-    message: err.message,
+    errors: [{ message: 'Something went wrong' }],
   });
 };
