6a947e54845d7bab214e638112550de67e7ca326
Michael Wells <mwells1286@gmail.com>
2020-05-11 19:18:44 -0700

    update json response.

--- auth/src/index.ts
+++ auth/src/index.ts
@@ -28,7 +28,11 @@ app.all('*', async (req, res) => {
   throw new NotFoundError();
 });
 app.use(errorHandler);
+
 const start = async () => {
+  if (!process.env.JWT_KEY) {
+    throw new Error('JWT_KEY undifined');
+  }
   try {
     mongoose.connect('mongodb://auth-mongo-srv:27017/auth', {
       useNewUrlParser: true,
