408198fd77f165762fe19ae9642fb4064bd272b1
Michael Wells <mwells1286@gmail.com>
2020-05-14 16:02:47 -0700

    add build client functionality

--- client/pages/index.js
+++ client/pages/index.js
@@ -1,27 +1,14 @@
-import axios from 'axios';
+import buildClient from '../api/build-client';
 
 const LandingPage = ({ currentUser }) => {
   console.log(currentUser);
   return <h1 className='title'>Landing Page</h1>;
 };
 
-LandingPage.getInitialProps = async ({ req }) => {
-  if (typeof window === 'undefined') {
-    //window only on clientside.
-    //Serverside
-    const { data } = await axios.get(
-      'http://ingress-nginx-controller.ingress-nginx.svc.cluster.local/api/users/currentuser',
-      {
-        headers: req.headers,
-      }
-    );
-    return data;
-  } else {
-    //clientside
-    const { data } = await axios.get('/api/users/currentuser');
-    return data;
-  }
-  return {};
+LandingPage.getInitialProps = async (context) => {
+  const client = buildClient(context);
+  const { data } = await client.get('/api/users/currentuser');
+  return data;
 };
 
 export default LandingPage;
