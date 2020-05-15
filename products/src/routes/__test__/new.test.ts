f4b921fe2ceb481eebc761ccaecd4fba61998108
Michael Wells <mwells1286@gmail.com>
2020-05-15 12:18:03 -0700

    add test for product creation auth

--- products/src/routes/__test__/new.test.ts
+++ products/src/routes/__test__/new.test.ts
@@ -5,7 +5,9 @@ it('has a route handler to /api/products for post request', async () => {
   const response = await request(app).post('/api/products').send({});
   expect(response.status).not.toEqual(404);
 });
-it('can be accesses only by auth user', async () => {});
+it('can be accesses only by auth user', async () => {
+  await request(app).post('/api/products').send({}).expect(401);
+});
 it('returns err for invalid title', async () => {});
 it('returns error for invalid price', async () => {});
 it('creates ticket if inputs are valid', async () => {});
