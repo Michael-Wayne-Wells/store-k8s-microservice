c5949ae606fd69499b2f59411c69fea0359328ad
Michael Wells <mwells1286@gmail.com>
2020-05-15 13:10:26 -0700

    add test for product route

--- products/src/routes/__test__/new.test.ts
+++ products/src/routes/__test__/new.test.ts
@@ -8,6 +8,54 @@ it('has a route handler to /api/products for post request', async () => {
 it('can be accesses only by auth user', async () => {
   await request(app).post('/api/products').send({}).expect(401);
 });
-it('returns err for invalid title', async () => {});
-it('returns error for invalid price', async () => {});
-it('creates ticket if inputs are valid', async () => {});
+it('return a status other than 401 if user is signed in', async () => {
+  const response = await request(app)
+    .post('/api/products')
+    .set('Cookie', global.signup())
+    .send({});
+  expect(response.status).not.toEqual(401);
+});
+it('returns err for invalid title', async () => {
+  await request(app)
+    .post('/api/products')
+    .set('Cookie', global.signup())
+    .send({
+      title: '',
+      price: 10,
+    })
+    .expect(400);
+  await request(app)
+    .post('/api/products')
+    .set('Cookie', global.signup())
+    .send({
+      price: 10,
+    })
+    .expect(400);
+});
+it('returns error for invalid price', async () => {
+  await request(app)
+    .post('/api/products')
+    .set('Cookie', global.signup())
+    .send({
+      title: 'skdjfks',
+      price: -10,
+    })
+    .expect(400);
+  await request(app)
+    .post('/api/products')
+    .set('Cookie', global.signup())
+    .send({
+      title: 'skdjfks',
+    })
+    .expect(400);
+});
+it('creates ticket if inputs are valid', async () => {
+  await request(app)
+    .post('/api/products')
+    .set('Cookie', global.signup())
+    .send({
+      title: 'skdjfks',
+      price: 10,
+    })
+    .expect(201);
+});
