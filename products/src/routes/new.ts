bb89f1f2bc13da388432706db0b80b34ccb57e56
Michael Wells <mwells1286@gmail.com>
2020-05-15 13:10:18 -0700

    add product route

--- products/src/routes/new.ts
+++ products/src/routes/new.ts
@@ -1,9 +1,18 @@
 import express, { Request, Response } from 'express';
-
+import { requireAuth, validateRequest } from '@mwproducts/common';
+import { body } from 'express-validator';
 const router = express.Router();
 
-router.post('/api/products', (req: Request, res: Response) => {
-  res.sendStatus(200);
-});
+router.post(
+  '/api/products',
+  requireAuth,
+  [body('title').not().isEmpty().withMessage('Title is required')],
+  body('price').isFloat({ gt: 0 }).withMessage('Price must be greater that 0'),
+
+  validateRequest,
+  (req: Request, res: Response) => {
+    res.sendStatus(200);
+  }
+);
 
 export { router as createProductRouter };
