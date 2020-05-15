d97d013a5e800566784a69658152ebb87be3f657
Michael Wells <mwells1286@gmail.com>
2020-05-15 13:09:58 -0700

    att product err handeling

--- products/src/app.ts
+++ products/src/app.ts
@@ -2,7 +2,7 @@ import express from 'express';
 import 'express-async-errors';
 import { json } from 'body-parser';
 import cookieSession from 'cookie-session';
-import { errorHandler, NotFoundError } from '@mwproducts/common';
+import { errorHandler, NotFoundError, currentUser } from '@mwproducts/common';
 import { createProductRouter } from './routes/new';
 
 const app = express();
@@ -14,7 +14,7 @@ app.use(
     secure: process.env.NODE_ENV !== 'test',
   })
 );
-
+app.use(currentUser);
 app.use(createProductRouter);
 app.all('*', async (req, res) => {
   throw new NotFoundError();
