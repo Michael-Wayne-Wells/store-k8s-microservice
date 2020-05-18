d7ce8c8f79269df1a6d4ce12c1c7c0b3df0b6a98
Michael Wells <mwells1286@gmail.com>
2020-05-18 09:57:02 -0700

    import product form mongo and check if exists

--- orders/src/routes/new.ts
+++ orders/src/routes/new.ts
@@ -1,6 +1,12 @@
 import mongoose from 'mongoose';
 import express, { Request, Response } from 'express';
-import { requireAuth, validateRequest } from '@mwproducts/common';
+import {
+  requireAuth,
+  validateRequest,
+  NotFoundError,
+} from '@mwproducts/common';
+import { Product } from '../models/product';
+import { Order } from '../models/order';
 import { body } from 'express-validator';
 const router = express.Router();
 
@@ -16,6 +22,20 @@ router.post(
   ],
   validateRequest,
   async (req: Request, res: Response) => {
+    const { productId } = req.body;
+    // find product user is trying to find in DB
+    const product = await Product.findById(productId);
+    if (!product) {
+      throw new NotFoundError();
+    }
+    // check if ticket is reserved
+
+    // Calculate an expiration date
+
+    // Build an order and save to DB
+
+    // publish an event that an order was created
+
     res.send({});
   }
 );
