e82d0bcd9e1220d2efa237502a2e8e9fbddb6553
Michael Wells <mwells1286@gmail.com>
2020-05-17 20:37:25 -0700

    add resolve line to nats

--- products/src/nats-wrapper.ts
+++ products/src/nats-wrapper.ts
@@ -16,6 +16,7 @@ class NatsWrapper {
     return new Promise((resolve, reject) => {
       this.client.on('connect', () => {
         console.log('Connected to nats');
+        resolve();
       });
       this.client.on('error', (err) => {
         reject(err);
