5898b49db07eab98b26567b7418b8edcd57c2483
Michael Wells <mwells1286@gmail.com>
2020-05-19 21:05:50 -0700

    import publisher and listenrs into index

--- payments/src/routes/new.ts
+++ payments/src/routes/new.ts
@@ -11,6 +11,8 @@ import {
 import { Order } from '../models/order';
 import { stripe } from '../stripe';
 import { Payment } from '../models/payments';
+import { natsWrapper } from '../nats-wrapper';
+import { PaymentCreatedPublisher } from '../events/publishers/payment-created-publisher';
 
 const router = express.Router();
 
@@ -43,7 +45,13 @@ router.post(
       stripeId: charge.id,
     });
     await payment.save();
-    res.status(201).send({ success: true });
+
+    await new PaymentCreatedPublisher(natsWrapper.client).publish({
+      id: payment.id,
+      orderId: payment.orderId,
+      stripeId: payment.stripeId,
+    });
+    res.status(201).send({ id: payment.id });
   }
 );
 
