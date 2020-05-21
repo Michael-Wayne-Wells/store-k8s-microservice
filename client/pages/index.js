d9124c40fabff66d93a9e8e17de3d3e1db5c15ff
Michael Wells <mwells1286@gmail.com>
2020-05-20 18:13:57 -0700

    add product listings to lander

--- client/pages/index.js
+++ client/pages/index.js
@@ -3,32 +3,36 @@ import Link from 'next/link';
 const LandingPage = ({ currentUser, products }) => {
   const productList = products.map((product) => {
     return (
-      <tr key={product.id}>
-        <td>{product.title}</td>
-        <td>{product.price}</td>
-        <td>
+      <div className='is-parent is-4' style={{ padding: '10px' }}>
+        <article className='tile is-child box' key={product.id}>
+          <h3 className='title is-size-3'>{product.title}</h3>
+          <h3 className='subtitle is-size-3'>$ {product.price}</h3>
           <Link href='/products/[productId]' as={`/products/${product.id}`}>
-            <a>View</a>
+            <button className='button is-primary'>View</button>
           </Link>
-        </td>
-      </tr>
+          <Link
+            href='/products/[productId]/edit'
+            as={`/products/${product.id}/edit`}
+          >
+            <button className='button is-warning'>Edit</button>
+          </Link>
+          <Link
+            href='/products/[productId]/delete'
+            as={`/products/${product.id}/delete`}
+          >
+            <button className='button is-danger'>Delete</button>
+          </Link>
+        </article>
+      </div>
     );
   });
 
   return (
-    <div>
-      <h2>Products</h2>
-      <table className='table'>
-        <thead>
-          <tr>
-            <th>Title</th>
-            <th>Price</th>
-            <th>Link</th>
-          </tr>
-        </thead>
-        <tbody>{productList}</tbody>
-      </table>
-    </div>
+    <section className='section'>
+      <h2 className='title is-size-2'>Products</h2>
+
+      <div className='tile is-ancestor'>{productList}</div>
+    </section>
   );
 };
 
