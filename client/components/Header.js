2be2f94d93b1bd6ce504e612386417507399018b
Michael Wells <mwells1286@gmail.com>
2020-05-20 18:12:33 -0700

    add more functionality to header

--- client/components/Header.js
+++ client/components/Header.js
@@ -1,9 +1,10 @@
 import Link from 'next/link';
 export default ({ currentUser }) => {
+  const [isActive, setisActive] = React.useState(false);
   const links = [
     !currentUser && { label: 'Sign Up', href: '/auth/signup' },
     !currentUser && { label: 'Sign In', href: '/auth/signin' },
-    currentUser && { label: 'List Products', href: '/products/new' },
+    currentUser && { label: 'Sell Something', href: '/products/new' },
     currentUser && { label: 'My Orders', href: '/orders' },
     currentUser && { label: 'Sign Out', href: '/auth/signout' },
   ]
@@ -17,16 +18,29 @@ export default ({ currentUser }) => {
     });
   return (
     <nav
-      className='navbar is-dark'
+      className='navbar is-primary'
       role='navigation'
       aria-label='main navigation'
     >
       <div className='navbar-brand'>
         <Link href='/'>
-          <a className='navbar-item title is-1 is-light'>Aleatory</a>
+          <a className='navbar-item title is-1 is-light main-font'>Aleatory</a>
         </Link>
+        <a
+          onClick={() => {
+            setisActive(!isActive);
+          }}
+          role='button'
+          className={`navbar-burger burger ${isActive ? 'is-active' : ''}`}
+          aria-label='menu'
+          aria-expanded='false'
+        >
+          <span aria-hidden='true'></span>
+          <span aria-hidden='true'></span>
+          <span aria-hidden='true'></span>
+        </a>
       </div>
-      <div className='navbar-menu'>
+      <div className={`navbar-menu ${isActive ? 'is-active' : ''}`}>
         <div className='navbar-start'>{links}</div>
       </div>
     </nav>
