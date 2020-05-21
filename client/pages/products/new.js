16b01021387c1d2198def809d174b065a267d31c
Michael Wells <mwells1286@gmail.com>
2020-05-20 18:15:36 -0700

    update to bulma for style on new

--- client/pages/products/new.js
+++ client/pages/products/new.js
@@ -32,30 +32,32 @@ const NewProduct = () => {
   };
 
   return (
-    <div>
-      <h1 className='title is-size-2'>Create a Product</h1>
-      <form onSubmit={onSubmit}>
-        <div className='form-group'>
-          <label>Title</label>
-          <input
-            value={title}
-            onChange={(e) => setTitle(e.target.value)}
-            className='form-control'
-          />
-        </div>
-        <div className='form-group'>
-          <label>Price</label>
-          <input
-            value={price}
-            onBlur={onBlur}
-            onChange={(e) => setPrice(e.target.value)}
-            className='form-control'
-          />
-        </div>
-        {errors}
-        <button className='btn btn-primary'>Submit</button>
-      </form>
-    </div>
+    <section className='section'>
+      <div className='box'>
+        <h1 className='title is-size-2'>Create a Product</h1>
+        <form onSubmit={onSubmit}>
+          <div className='field'>
+            <label className='label'>Title</label>
+            <input
+              value={title}
+              onChange={(e) => setTitle(e.target.value)}
+              className='control input'
+            />
+          </div>
+          <div className='field'>
+            <label className='label'>Price</label>
+            <input
+              value={price}
+              onBlur={onBlur}
+              onChange={(e) => setPrice(e.target.value)}
+              className='control input'
+            />
+          </div>
+          {errors}
+          <button className='button is-primary'>Submit</button>
+        </form>
+      </div>
+    </section>
   );
 };
 
